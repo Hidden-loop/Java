d1935976a54c96c490469ec4f0faad276846dc76
Hidden <113853976+Hidden-loop@users.noreply.github.com>
2023-05-04 23:32:23 +0530

    Update AbDisp.java

--- AbDisp.java
+++ AbDisp.java
@@ -10,23 +10,17 @@ public class AbDisp {
         a.setLayout(null);
         a.setVisible(true);
 
-        JLabel n0, n1, n2;
+        JLabel n0, n1;
         n0 = new JLabel("made by:");
         n0.setFont(new Font("Viner Hand ITC", Font.BOLD, 25));
         n0.setBounds(100, 50, 150, 20);
         a.add(n0);
 
-        n1 = new JLabel("Shrushti Gupta");
+        n1 = new JLabel("name");
         n1.setFont(new Font("Thomas", Font.PLAIN, 17));
         n1.setBounds(20, 150, 150, 20);
         n1.setVisible(true);
         a.add(n1);
-
-        n2 = new JLabel("Saikrishna Agrawal");
-        n2.setFont(new Font("Thomas", Font.PLAIN, 17));
-        n2.setBounds(170, 150, 150, 20);
-        n2.setVisible(true);
-        a.add(n2);
 
     }
 
